ebfd425be849207b97b5ef4b5deaf18e57914f92
mrafiaenal-pixel <mrafiaenal@gmail.com>
2026-09-24 16:35:24 +0700

    hshshsh

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Penanganan navigasi internal yang mulus
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetElement = document.querySelector(this.getAttribute("href"));
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
+});
